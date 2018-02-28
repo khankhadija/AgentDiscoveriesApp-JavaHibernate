30c02d6389a513f60a1b975b25eeddd2588ba0dd
JackMead <Jack.Mead@softwire.com>
2018-02-28 17:02:03 +0000

    Added test to check submitting as admin field

--- AgentDiscoveries-Frontend/app/test/test_submit_utilities.js
+++ AgentDiscoveries-Frontend/app/test/test_submit_utilities.js
@@ -18,21 +18,26 @@ describe("submit utilities", () => {
     const submitForm = {"locations": {value: aValue}};
     expect(utilities.getBodyJSON(submitForm)).to.deep.equal({"locations": [10, 2, 14]});
   });
-  //TODO
-  //ADD test for new admin condition change
 
-  it('should be able to combine locations with non-locations', () => {
+  it('should transform admin-key value to true if on',()=>{
+    const submitAdmin = {"admin": {value: "on"}};
+    const submitNonAdmin = {"admin": {value: "something else"}};
+    expect(utilities.getBodyJSON(submitAdmin)).to.deep.equal({"admin":true});
+    expect(utilities.getBodyJSON(submitNonAdmin)).to.deep.equal({"admin":false});
+  })
+
+  it('should be able to combine these with multiple fields', () => {
     const submitForm = {
       "notLocation": {value: "someValue"},
-      "name": {value: "myName"},
+      "name": {value: ""},
       "locations": {value: "17 12 41 1"},
-      "status": {value: ""}
+      "admin": {value: "on"}
     };
     const expectedResponse = {
       "notLocation": "someValue",
-      "name": "myName",
+      "name": "",
       "locations": [17, 12, 41, 1],
-      "status": ""
+      "admin": true
     };
     expect(utilities.getBodyJSON(submitForm)).to.deep.equal(expectedResponse);
 
